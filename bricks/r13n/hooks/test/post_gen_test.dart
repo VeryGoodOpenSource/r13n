6667c7255dd5a2d5247c5bed9ef834ce63165779
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2022-08-16 15:38:00 +0200

    feat: r13n code generation

--- bricks/r13n/hooks/test/post_gen_test.dart
+++ bricks/r13n/hooks/test/post_gen_test.dart
@@ -1,20 +1,60 @@
+import 'dart:io';
+
 import 'package:mason/mason.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 import '../post_gen.dart' as post_gen;
 
+class _TestProcess {
+  Future<ProcessResult> run(
+    String command,
+    List<String> args, {
+    bool runInShell = false,
+    String? workingDirectory,
+  }) {
+    throw UnimplementedError();
+  }
+}
+
 class _MockHookContext extends Mock implements HookContext {}
+
+class _MockProcess extends Mock implements _TestProcess {}
+
+class _MockProcessResult extends Mock implements ProcessResult {}
 
 void main() {
   group('post_gen', () {
     late HookContext hookContext;
+    late _TestProcess process;
+    late ProcessResult processResult;
 
     setUp(() {
       hookContext = _MockHookContext();
+      process = _MockProcess();
+      processResult = _MockProcessResult();
+      when(() => processResult.exitCode).thenReturn(ExitCode.success.code);
+      when(
+        () => process.run(
+          any(),
+          any(),
+          runInShell: any(named: 'runInShell'),
+          workingDirectory: any(named: 'workingDirectory'),
+        ),
+      ).thenAnswer((_) async => processResult);
     });
 
     test('returns normally', () {
-      expect(() => post_gen.run(hookContext), returnsNormally);
+      post_gen.ProcessOverrides.runZoned(() {
+        expect(() => post_gen.run(hookContext), returnsNormally);
+      }, runProcess: process.run);
+    });
+
+    test('throws exception if format fails', () {
+      when(() => processResult.exitCode).thenReturn(ExitCode.osError.code);
+
+      post_gen.ProcessOverrides.runZoned(() {
+        expectLater(() => post_gen.run(hookContext), throwsA(isA<Exception>()));
+      }, runProcess: process.run);
     });
   });
 }
