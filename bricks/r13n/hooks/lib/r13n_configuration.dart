1edf8bb360762e622dc9e4b977b2b2e2fc58c284
Alejandro Santiago <dev@alestiago.com>
2023-03-06 20:05:48 +0000

    feat: included R13nYamlParseException

--- bricks/r13n/hooks/lib/r13n_configuration.dart
+++ bricks/r13n/hooks/lib/r13n_configuration.dart
@@ -1,14 +1,15 @@
 import 'dart:io';
 
+import 'package:meta/meta.dart';
 import 'package:path/path.dart' as path;
 import 'package:r13n_hooks/hooks.dart';
 import 'package:yaml/yaml.dart';
 
 /// {@template r13n_yaml_not_found_exception}
 /// Exception thrown when [R13nConfiguration] fails to find
-/// [R13nConfiguration._fileName] file.
+/// [R13nConfiguration.fileName] file.
 ///
-/// A [R13nConfiguration._fileName] file is required to define the location of
+/// A [R13nConfiguration.fileName] file is required to define the location of
 /// the arb files and other configurations.
 /// {@endtemplate}
 class R13nYamlNotFoundException extends R13nException {
@@ -16,10 +17,19 @@ class R13nYamlNotFoundException extends R13nException {
   R13nYamlNotFoundException() : super(message: 'No r13n.yaml found.');
 }
 
+/// {@template r13n_yaml_parse_exception}
+/// Exception thrown when [R13nConfiguration] fails to parse the
+/// [R13nConfiguration.fileName] file.
+/// {@endtemplate}
+class R13nYamlParseException extends R13nException {
+  /// {@macro r13n_yaml_parse_exception}
+  R13nYamlParseException(String message) : super(message: message);
+}
+
 /// {@template r13n_configuration}
 /// Defines the configuration for the r13n brick.
 ///
-/// The configuration is read from [R13nConfiguration._fileName] file. This
+/// The configuration is read from [R13nConfiguration.fileName] file. This
 /// should be a yaml file with the following structure:
 /// ```yaml
 /// arb-dir: lib/l10n # path relative to the file location indicating the location of the arb files
@@ -33,29 +43,48 @@ class R13nConfiguration {
     required this.templateArbFile,
   });
 
-  static const _fileName = 'r13n.yaml';
+  /// The name of the configuration file.
+  @visibleForTesting
+  static const fileName = 'r13n.yaml';
 
-  /// Reads the configuration from [R13nConfiguration._fileName] file.
+  /// Reads the configuration from [R13nConfiguration.fileName] file.
   ///
   /// Throws a [R13nYamlNotFoundException] if the file is not found.
   static Future<R13nConfiguration> read() async {
-    final file = File(path.join(Directory.current.path, _fileName));
+    final file = File(path.join(Directory.current.path, fileName));
     if (!file.existsSync()) {
       throw R13nYamlNotFoundException();
     }
 
-    final content = await file.readAsString();
-    final yaml = loadYaml(content) as YamlMap;
+    late final YamlMap yaml;
+    try {
+      final content = await file.readAsString();
+      yaml = loadYaml(content) as YamlMap;
+    } catch (e, stackTrace) {
+      throw R13nYamlParseException(
+        'Failed to parse $fileName file. \n'
+        'Failed with error: $e '
+        'Stack trace: $stackTrace',
+      );
+    }
 
     final arbDir = yaml['arb-dir'] as String?;
-    if (arbDir == null) {}
+    if (arbDir == null) {
+      throw R13nYamlParseException(
+        'Missing required field "arb-dir" in $fileName',
+      );
+    }
 
     final templateArbFile = yaml['template-arb-file'] as String?;
-    if (templateArbFile == null) {}
+    if (templateArbFile == null) {
+      throw R13nYamlParseException(
+        'Missing required field "template-arb-file" in $fileName',
+      );
+    }
 
     return R13nConfiguration._(
-      arbDir: arbDir!,
-      templateArbFile: templateArbFile!,
+      arbDir: arbDir,
+      templateArbFile: templateArbFile,
     );
   }
 
@@ -65,7 +94,7 @@ class R13nConfiguration {
   /// directly under this directory.
   ///
   /// The path value is relative to the location of
-  /// [R13nConfiguration._fileName] file.
+  /// [R13nConfiguration.fileName] file.
   ///
   /// For example:
   /// ```yaml
--- bricks/r13n/hooks/test/lib/r13n_configuration_test.dart
+++ bricks/r13n/hooks/test/lib/r13n_configuration_test.dart
@@ -8,46 +8,108 @@ class _MockFile extends Mock implements File {}
 
 void main() {
   group('$R13nConfiguration', () {
-    late File configFile;
+    group('read', () {
+      late File configFile;
 
-    setUp(() {
-      configFile = _MockFile();
-      when(() => configFile.readAsString()).thenAnswer(
-        (_) async => '''
+      setUp(() {
+        configFile = _MockFile();
+        const configFileContent = '''
 arb-dir: ARB_DIR
 template-arb-file: TEMPLATE_ARB_FILE
-''',
-      );
-      when(() => configFile.existsSync()).thenReturn(true);
-    });
+''';
+        when(() => configFile.readAsString()).thenAnswer(
+          (_) async => configFileContent,
+        );
+        when(() => configFile.existsSync()).thenReturn(true);
+      });
 
-    test('read yaml configuration', () async {
-      await IOOverrides.runZoned(
-        () async {
-          final configuration = await R13nConfiguration.read();
+      test('read yaml configuration', () async {
+        await IOOverrides.runZoned(
+          () async {
+            final configuration = await R13nConfiguration.read();
 
-          expect(configuration.arbDir, equals('ARB_DIR'));
-          expect(configuration.templateArbFile, equals('TEMPLATE_ARB_FILE'));
+            expect(configuration.arbDir, equals('ARB_DIR'));
+            expect(configuration.templateArbFile, equals('TEMPLATE_ARB_FILE'));
 
-          verify(() => configFile.readAsString()).called(1);
-        },
-        createFile: (path) => configFile,
-      );
-    });
+            verify(() => configFile.readAsString()).called(1);
+          },
+          createFile: (path) => configFile,
+        );
+      });
 
-    test('throws $R13nYamlNotFoundException when something goes wrong',
-        () async {
-      await IOOverrides.runZoned(
-        () async {
-          await expectLater(
-            R13nConfiguration.read,
-            throwsA(isA<R13nYamlNotFoundException>()),
-          );
+      test('throws $R13nYamlParseException when something goes wrong',
+          () async {
+        await IOOverrides.runZoned(
+          createFile: (path) => configFile,
+          () async {
+            when(() => configFile.readAsString()).thenAnswer(
+              (_) async => throw const FileSystemException(),
+            );
+            await expectLater(
+              R13nConfiguration.read,
+              throwsA(isA<R13nYamlParseException>()),
+            );
+          },
+        );
+      });
 
-          verifyNever(() => configFile.readAsString());
-        },
-        createFile: (path) => throw const FileSystemException(),
-      );
+      test('throws $R13nYamlParseException when "arb-dir" is not provided',
+          () async {
+        await IOOverrides.runZoned(
+          createFile: (path) => configFile,
+          () async {
+            const configFileContent = '''
+template-arb-file: TEMPLATE_ARB_FILE
+''';
+            when(() => configFile.readAsString()).thenAnswer(
+              (_) async => configFileContent,
+            );
+
+            const errorMessage = 'Missing required field "arb-dir" in '
+                '${R13nConfiguration.fileName}';
+            await expectLater(
+              R13nConfiguration.read,
+              throwsA(
+                isA<R13nYamlParseException>().having(
+                  (exception) => exception.message,
+                  'message',
+                  equals(errorMessage),
+                ),
+              ),
+            );
+          },
+        );
+      });
+
+      test(
+          'throws $R13nYamlParseException when "template-arb-file" is not '
+          'provided', () async {
+        await IOOverrides.runZoned(
+          createFile: (path) => configFile,
+          () async {
+            const configFileContent = '''
+arb-dir: ARB_DIR
+''';
+            when(() => configFile.readAsString()).thenAnswer(
+              (_) async => configFileContent,
+            );
+
+            const errorMessage =
+                'Missing required field "template-arb-file" in '
+                '${R13nConfiguration.fileName}';
+            await expectLater(
+              R13nConfiguration.read,
+              throwsA(
+                isA<R13nYamlParseException>().having(
+                  (exception) => exception.message,
+                  'message',
+                  equals(errorMessage),
+                ),
+              ),
+            );
+          },
+        );
+      });
     });
   });
 }
