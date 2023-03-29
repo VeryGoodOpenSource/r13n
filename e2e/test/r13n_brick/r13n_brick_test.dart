3f4f5cd847a57db8fadba3af3350d41ff5b7259a
Alejandro Santiago <dev@alestiago.com>
2023-03-29 12:29:59 +0100

    test: e2e for r13n brick (#45)
    * test: initial e2e test
    
    * chore: very_good create -t dart_pkg e2e
    
    * chore: very_good create dart_package r13n_e2e
    
    * chore: removed tool/e2e
    
    * test: e2e
    
    * test: r13n_brick_test.dart
    
    * chore: removed coverage_badge.svg
    
    * chore: versions
    
    * test: added fixtures for e2e
    
    * docs: e2e tes
    
    * chore: e2e workflow
    
    * ci: updated run test
    
    * test: r13_brick_test.dart corrections
    
    * test: string fixes
    
    * refactor: typo
    
    * test: fixing r13n_brick_test.dart
    
    * ci: renamed path
    
    * test: reason renaming
    
    * chore: keep version untouched
    
    * chore: versioning
    
    * test: changed e2e test
    
    * test: added command param
    
    * test: r13_brick_test changes
    
    * chore: pubspec.yaml
    
    * test: formatted matcher
    
    * fix: r13n_brick_test working directories
    
    * chore: updated dependencies
    
    * chore: decreased analyis version
    
    * test: removed redundant space
    
    * test: removed cascade

---
+++ e2e/test/r13n_brick/r13n_brick_test.dart
@@ -0,0 +1,141 @@
+import 'dart:io';
+
+import 'package:mason/mason.dart';
+import 'package:path/path.dart' as path;
+import 'package:test/test.dart';
+
+/// Objectives:
+///
+/// * Generate AppRegionalizations from arb files (`mason make r13n`)
+/// * Ensure the code is formatted (`dart format . --set-exit-if-changed`)
+/// * Ensure the code has no warnings/errors (`dart analyze .`)
+/// * Ensure generated AppRegionalizations have valid members
+void main() {
+  test(
+    'r13n brick generates successfully',
+    timeout: const Timeout(Duration(minutes: 5)),
+    () async {
+      final tempDirectory = Directory.systemTemp.createTempSync();
+      final setUpPath =
+          path.join(Directory.current.path, 'test', 'r13n_brick', 'set_up');
+      final copyResult = await Process.run(
+        'cp',
+        ['-R', './', tempDirectory.path],
+        runInShell: true,
+        workingDirectory: setUpPath,
+      );
+      expect(
+        copyResult,
+        _isSuccessful,
+        reason: '`cp -R ./ ${tempDirectory.path}` at $setUpPath failed.',
+      );
+
+      final flutterPubGetResult = await Process.run(
+        'flutter',
+        ['pub', 'get'],
+        runInShell: true,
+        workingDirectory: tempDirectory.path,
+      );
+      expect(
+        flutterPubGetResult,
+        _isSuccessful,
+        reason: '`flutter pub get` at ${tempDirectory.path} failed.',
+      );
+
+      final rootPath = Directory.current.parent.path;
+      final r13nBrickPath = path.join(rootPath, 'bricks', 'r13n');
+      final r13nBrick = Brick.path(r13nBrickPath);
+      final r13nMasonGenerator = await MasonGenerator.fromBrick(r13nBrick);
+      final directoryGeneratorTarget = DirectoryGeneratorTarget(tempDirectory);
+
+      var vars = <String, dynamic>{};
+      await r13nMasonGenerator.hooks.preGen(
+        workingDirectory: tempDirectory.path,
+        onVarsChanged: (newVars) => vars = newVars,
+      );
+      await r13nMasonGenerator.generate(
+        directoryGeneratorTarget,
+        vars: vars,
+      );
+      await r13nMasonGenerator.hooks.postGen(
+        workingDirectory: tempDirectory.path,
+        vars: vars,
+      );
+
+      final dartFormatResult = await Process.run(
+        'dart',
+        ['format', '.', '--set-exit-if-changed'],
+        runInShell: true,
+        workingDirectory: tempDirectory.path,
+      );
+      expect(
+        dartFormatResult,
+        _isSuccessful,
+        reason:
+            '''`dart format . --set-exit-if-changed` at ${tempDirectory.path} failed.''',
+      );
+
+      final dartAnalyzeResult = await Process.run(
+        'dart',
+        ['analyze', '.'],
+        runInShell: true,
+        workingDirectory: tempDirectory.path,
+      );
+      expect(
+        dartAnalyzeResult,
+        _isSuccessful,
+        reason: '`dart analyze .` at ${tempDirectory.path} failed.',
+      );
+
+      final flutterTestResult = await Process.run(
+        'flutter',
+        ['test'],
+        runInShell: true,
+        workingDirectory: tempDirectory.path,
+      );
+      expect(
+        flutterTestResult,
+        _isSuccessful,
+        reason: '`flutter test` at ${tempDirectory.path} failed.',
+      );
+
+      tempDirectory.deleteSync(recursive: true);
+    },
+  );
+}
+
+const _isSuccessful = _ProcessResultSuccessfulMatcher();
+
+class _ProcessResultSuccessfulMatcher extends Matcher {
+  const _ProcessResultSuccessfulMatcher();
+
+  /// Exit code indicating a command completed successfully.
+  ///
+  /// [Source](https://www.freebsd.org/cgi/man.cgi?query=sysexits).
+  static const _sucessfulExitCode = 0;
+
+  @override
+  bool matches(covariant ProcessResult item, Map<dynamic, dynamic> matchState) {
+    final hasEmptyStderr = (item.stderr as String).isEmpty;
+    final hasSuccessfulExitCode = item.exitCode == _sucessfulExitCode;
+    return hasSuccessfulExitCode && hasEmptyStderr;
+  }
+
+  @override
+  Description describeMismatch(
+    covariant ProcessResult item,
+    Description mismatchDescription,
+    Map<dynamic, dynamic> matchState,
+    bool verbose,
+  ) {
+    return describe(
+      mismatchDescription.add(
+        '''Failed with exit code ${item.exitCode} and stderr: "${item.stderr}". ''',
+      ),
+    );
+  }
+
+  @override
+  Description describe(Description description) =>
+      description.addDescriptionOf('ProcessResult to run successfully.');
+}
---
+++ e2e/test/r13n_brick/set_up/test/r13n_test.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:r13n/r13n.dart';
+import 'package:r13n_e2e_test/r13n/arb/gen/app_regionalizations.g.dart';
+import 'package:r13n_e2e_test/r13n/arb/gen/app_regionalizations_gb.g.dart';
+import 'package:r13n_e2e_test/r13n/arb/gen/app_regionalizations_us.g.dart';
+
+void main() {
+  group('AppRegionalizationsGb', () {
+    test('has "gb" region', () {
+      final regionalizations = AppRegionalizationsGb();
+      final region = Region(regionalCode: 'gb');
+      expect(regionalizations.region, equals(region));
+    });
+
+    test('has "gb@verygood.ventures" email', () {
+      final regionalizations = AppRegionalizationsGb();
+      final supportEmail = 'gb@verygood.ventures';
+      expect(regionalizations.supportEmail, equals(supportEmail));
+    });
+  });
+
+  group('AppRegionalizationsUs', () {
+    test('has "us" region', () {
+      final regionalizations = AppRegionalizationsUs();
+      final region = Region(regionalCode: 'us');
+      expect(regionalizations.region, equals(region));
+    });
+
+    test('has "us@verygood.ventures" email', () {
+      final regionalizations = AppRegionalizationsUs();
+      final supportEmail = 'us@verygood.ventures';
+      expect(regionalizations.supportEmail, equals(supportEmail));
+    });
+  });
+
+  group('Regionalizations', () {
+    testWidgets('has "us" region as default', (tester) async {
+      late BuildContext buildContext;
+      await tester.pumpWidget(
+        StatefulBuilder(
+          builder: (context, _) {
+            buildContext = context;
+            return SizedBox();
+          },
+        ),
+      );
+
+      final region = Region(regionalCode: 'us');
+      expect(AppRegionalizations.of(buildContext).region, equals(region));
+    });
+  });
+}
